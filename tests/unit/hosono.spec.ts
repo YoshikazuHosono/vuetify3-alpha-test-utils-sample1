0a6c0eafb68df0936e2ba6a78f8a0f90491f4aa7
YoshikazuHosono <yyhhwork@gmail.com>
2022-04-01 15:28:32 +0900

    create

---
+++ tests/unit/hosono.spec.ts
@@ -0,0 +1,9 @@
+import { createVuetify } from 'vuetify';
+
+const tmp = createVuetify();
+
+describe('hosono', () => {
+  it('vuetify v-btn clicked', async () => {
+    expect('1').toBe('1');
+  });
+});
